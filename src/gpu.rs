eb23d1d77cf3681e012c0359612e92c26cfc5a06
ethanrutt <ethan.rutt2032@gmail.com>
2025-04-03 13:22:14 -0500

    add gpu info

---
+++ src/gpu.rs
@@ -0,0 +1,36 @@
+use std::process::Command;
+
+pub struct GpuInfo {
+    pub name: String,
+    pub mem_size: String,
+}
+
+pub fn read_gpuinfo() -> GpuInfo {
+    let process = Command::new("glxinfo")
+        .output()
+        .ok()
+        .expect("Failed to execute");
+    let contents = std::string::String::from_utf8(process.stdout)
+        .ok()
+        .expect("Failed to read");
+
+    let lines = contents.split("\n");
+
+    let mut gpu_info: GpuInfo = GpuInfo {
+        name: String::from("not parsed yet"),
+        mem_size: String::from("-1"),
+    };
+
+    for line in lines {
+        let cols = line.split(":").collect::<Vec<_>>();
+
+        if cols[0].trim() == "Device" {
+            gpu_info.name = cols[1].trim().parse().expect("device name not a string");
+        }
+        else if cols[0].trim() == "Video memory" {
+            gpu_info.mem_size = cols[1].trim().parse().expect("video memory not a string");
+        }
+    }
+
+    gpu_info
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 pub mod cpu;
+pub mod gpu;
+pub mod ram;
 
 fn main() {
     let core_info: cpu::CpuInfo = cpu::read_cpuinfo();
     for core in core_info.cores {
         println!("{}: {}", core.processor_number, core.ghz);
     }
+
+    let gpu_info: gpu::GpuInfo = gpu::read_gpuinfo();
+
+    println!("{}", gpu_info.name);
+    println!("{}", gpu_info.mem_size);
+
+    let ram_info: ram::RamInfo = ram::read_raminfo();
+
+    println!("{}", ram_info.total);
+    println!("{}", ram_info.free);
+    println!("{}", ram_info.available);
+    println!("{}", ram_info.cached);
 }
